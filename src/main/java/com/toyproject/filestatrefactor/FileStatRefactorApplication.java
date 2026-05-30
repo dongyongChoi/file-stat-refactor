8be6cd01992cfb48c398fcb93bf8946dea1209f6
choidongyong <cni274@gmail.com>
2026-05-30 19:50:04 +0900

    init

---
+++ src/main/java/com/toyproject/filestatrefactor/FileStatRefactorApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.filestatrefactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStatRefactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileStatRefactorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toyproject/filestatrefactor/FileStatRefactorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.filestatrefactor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStatRefactorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
